10fb1c7be9f6b9b6486f315af0ee338d576ea615
Antrinh <antrinh@gmail.com>
2025-06-20 22:29:01 +0700

    modified sth

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -13,23 +13,23 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(800, 444)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.textBrowser_2 = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser_2.setGeometry(QtCore.QRect(30, 140, 361, 101))
+        self.textBrowser_2.setGeometry(QtCore.QRect(30, 130, 361, 71))
         self.textBrowser_2.setStyleSheet("background: transparent;\n"
 "border: none;\n"
 "color: black;  ")
         self.textBrowser_2.setObjectName("textBrowser_2")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(30, 250, 361, 31))
+        self.pushButton.setGeometry(QtCore.QRect(30, 210, 361, 41))
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(30, 290, 361, 31))
+        self.pushButton_2.setGeometry(QtCore.QRect(410, 210, 361, 41))
         self.pushButton_2.setObjectName("pushButton_2")
         self.splitter = QtWidgets.QSplitter(self.centralwidget)
-        self.splitter.setGeometry(QtCore.QRect(100, 380, 601, 101))
+        self.splitter.setGeometry(QtCore.QRect(110, 270, 581, 71))
         self.splitter.setOrientation(QtCore.Qt.Horizontal)
         self.splitter.setObjectName("splitter")
         self.drop_red = QtWidgets.QFrame(self.splitter)
@@ -38,7 +38,7 @@ class Ui_MainWindow(object):
         self.drop_red.setFrameShadow(QtWidgets.QFrame.Raised)
         self.drop_red.setObjectName("drop_red")
         self.label = QtWidgets.QLabel(self.drop_red)
-        self.label.setGeometry(QtCore.QRect(100, 40, 67, 17))
+        self.label.setGeometry(QtCore.QRect(90, 30, 16, 17))
         self.label.setObjectName("label")
         self.drop_green = QtWidgets.QFrame(self.splitter)
         self.drop_green.setStyleSheet("background-color: rgb(138, 226, 52);")
@@ -46,7 +46,7 @@ class Ui_MainWindow(object):
         self.drop_green.setFrameShadow(QtWidgets.QFrame.Raised)
         self.drop_green.setObjectName("drop_green")
         self.label_2 = QtWidgets.QLabel(self.drop_green)
-        self.label_2.setGeometry(QtCore.QRect(100, 40, 16, 17))
+        self.label_2.setGeometry(QtCore.QRect(90, 30, 16, 17))
         self.label_2.setObjectName("label_2")
         self.drop_blue = QtWidgets.QFrame(self.splitter)
         self.drop_blue.setStyleSheet("background-color: rgb(52, 101, 164);")
@@ -54,15 +54,15 @@ class Ui_MainWindow(object):
         self.drop_blue.setFrameShadow(QtWidgets.QFrame.Raised)
         self.drop_blue.setObjectName("drop_blue")
         self.label_3 = QtWidgets.QLabel(self.drop_blue)
-        self.label_3.setGeometry(QtCore.QRect(90, 40, 67, 17))
+        self.label_3.setGeometry(QtCore.QRect(90, 30, 16, 17))
         self.label_3.setObjectName("label_3")
-        self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(120, 10, 591, 161))
-        self.widget.setObjectName("widget")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.widget)
+        self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.layoutWidget.setGeometry(QtCore.QRect(120, 0, 591, 111))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.layoutWidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.textBrowser = QtWidgets.QTextBrowser(self.widget)
+        self.textBrowser = QtWidgets.QTextBrowser(self.layoutWidget)
         font = QtGui.QFont()
         font.setFamily("Ubuntu Mono")
         font.setBold(True)
@@ -73,12 +73,15 @@ class Ui_MainWindow(object):
 "color: black;  ")
         self.textBrowser.setObjectName("textBrowser")
         self.verticalLayout.addWidget(self.textBrowser)
-        self.textBrowser_3 = QtWidgets.QTextBrowser(self.widget)
+        self.textBrowser_3 = QtWidgets.QTextBrowser(self.layoutWidget)
         self.textBrowser_3.setStyleSheet("background: transparent;\n"
 "border: none;\n"
 "color: black;")
         self.textBrowser_3.setObjectName("textBrowser_3")
         self.verticalLayout.addWidget(self.textBrowser_3)
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(30, 350, 451, 41))
+        self.textEdit.setObjectName("textEdit")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
@@ -98,9 +101,7 @@ class Ui_MainWindow(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Sinh viên thực hiện:</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br />Hà Tuấn Phong 20210677</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br />Trịnh Vũ An 20216033</p></body></html>"))
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Sinh viên thực hiện:<br />Hà Tuấn Phong 20210677<br />Trịnh Vũ An 20216033</p></body></html>"))
         self.pushButton.setText(_translate("MainWindow", "Bắt đầu phân loại"))
         self.pushButton_2.setText(_translate("MainWindow", "Reset"))
         self.label.setText(_translate("MainWindow", "0"))
@@ -111,10 +112,9 @@ class Ui_MainWindow(object):
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Ubuntu Mono\'; font-size:11pt; font-weight:600; font-style:normal;\">\n"
 "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt; color:#ef2929;\">HANOI UNIVERSITY OF SCIENCE AND TECHNOLOGY</span></p>\n"
-"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:16pt; color:#ef2929;\"><br /></p>\n"
 "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt; color:#ef2929;\">SCHOOL OF MECHANICAL ENGINEERING</span></p></body></html>"))
         self.textBrowser_3.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt;\">Mô hình cánh tay robot tích hợp công nghệ xử lý ảnh</span></p></body></html>"))
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">Mô hình cánh tay robot tích hợp công nghệ xử lý ảnh</span></p></body></html>"))
